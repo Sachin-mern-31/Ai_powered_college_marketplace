c8488de6784212872ccd8c147fcdc51c759ed114
Sachin-mern-31 <mernstack37382@gmail.com>
2026-09-08 16:17:07 +0530

    chore: remove redundant files (root vercel.json, api/, .npmrc, plan.md)

--- api/index.js
+++
@@ -1,9 +0,0 @@
-import app from '../server/server.js';
-import { connectDB } from '../server/config/db.js';
-
-// Connect to MongoDB or In-Memory Database on Vercel Serverless invocation
-connectDB().catch((err) => {
-  console.warn('[Vercel Serverless DB Warning]', err.message);
-});
-
-export default app;
